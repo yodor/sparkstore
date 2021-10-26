27ce5d6de1608bf2eacb85fec85be107cabbf2eb
Todor Gyumyushev <yodor1@gmail.com>
2021-10-26 04:01:00 +0300

    updates
    Signed-off-by: Todor Gyumyushev <yodor1@gmail.com>

---
+++ lib/store/pages/AdminPageBase.php
@@ -0,0 +1,21 @@
+<?php
+include_once("pages/SparkAdminPage.php");
+include_once("components/MenuBarComponent.php");
+
+include_once("components/renderers/cells/BeanFieldCellRenderer.php");
+include_once("components/renderers/cells/CallbackCellRenderer.php");
+include_once("components/renderers/cells/BooleanCellRenderer.php");
+
+class AdminPageBase extends SparkAdminPage
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->addCSS(STORE_LOCAL . "/css/AdminPage.css");
+    }
+
+}
+
+?>
