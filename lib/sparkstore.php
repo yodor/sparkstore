5e9f4b2b74325ed51aeeca67e3de3f056da2c4b2
Todor Gyumyushev <yodor1@gmail.com>
2023-03-05 16:46:09 +0200

    updates: set default origin logo
    Signed-off-by: Todor Gyumyushev <yodor1@gmail.com>

--- lib/sparkstore.php
+++ lib/sparkstore.php
@@ -9,6 +9,8 @@ $location = $globals->get("LOCAL");
 
 //sparkbox frontend classes location (js/css/images) - HTTP accessible - without ending slash
 $globals->set("STORE_LOCAL", $location . "/storefront");
+$globals->set("LOGO_NAME", "logo_header.svg");
+$globals->set("LOGO_PATH", $globals->get("INSTALL_PATH")."/storefront/images/");
 
 $globals->export();
 
