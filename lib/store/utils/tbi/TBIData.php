74180a345de4e12d5c53047282f78fb82fe47d5e
Todor Gyumyushev <yodor1@gmail.com>
2023-03-28 14:46:31 +0300

    updates tbi
    Signed-off-by: Todor Gyumyushev <yodor1@gmail.com>

--- lib/store/utils/tbi/TBIData.php
+++ lib/store/utils/tbi/TBIData.php
@@ -9,4 +9,17 @@ class TBIData
     public static $store_uid;
 }
 
+function isProductInCategories($categories_id=array(), $prod_categories_id=array()) {
+    if ($categories_id[0] != ""){
+        if ($prod_categories_id[0] != 0){
+            foreach ($prod_categories_id as $prod_category_id){
+                if (in_array($prod_category_id,$categories_id)){
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
+
 ?>
