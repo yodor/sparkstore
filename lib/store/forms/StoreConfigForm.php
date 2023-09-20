15468b3fba8f71bea5d215852b2211b3a003ed50
Todor Gyumyushev <yodor1@gmail.com>
2023-09-20 23:20:41 +0300

    updates banner home
    Signed-off-by: Todor Gyumyushev <yodor1@gmail.com>

--- lib/store/forms/StoreConfigForm.php
+++ lib/store/forms/StoreConfigForm.php
@@ -10,41 +10,54 @@ class StoreConfigForm extends InputForm
 
         parent::__construct();
 
-        $field = DataInputFactory::Create(DataInputFactory::TEXTAREA, "phone_orders", "Phone Orders", 0);
-        $this->addInput($field);
+        $grp_general = new InputGroup("general", "General");
+        $this->addGroup($grp_general);
+        $field = DataInputFactory::Create(DataInputFactory::TEXT, "phone_orders", "Phone (For receiving orders)", 0);
+        $this->addInput($field, $grp_general);
+
+        $field = DataInputFactory::Create(DataInputFactory::TEXT, "tbi_uid", "TBI Store UID", 0);
+        $this->addInput($field, $grp_general);
 
         $field = DataInputFactory::Create(DataInputFactory::TEXTAREA, "marquee_text", "Header Marquee Text", 0);
-        $this->addInput($field);
+        $this->addInput($field, $grp_general);
 
-        $field = DataInputFactory::Create(DataInputFactory::TEXTAREA, "tbi_uid", "TBI Store UID", 0);
-        $this->addInput($field);
+        $field = DataInputFactory::Create(DataInputFactory::TEXT, "facebook_page_id", "Facebook Page ID (Enable Facebook chat Plugin)", 0);
+        $this->addInput($field, $grp_general);
+
+        $field = DataInputFactory::Create(DataInputFactory::SESSION_IMAGE, "home_banner_popup", "Banner Image (Home page banner popup)", 0);
+        $this->addInput($field, $grp_general);
+
+
+        $grp_footer = new InputGroup("footerButtons", "Site Footer - Round Buttons");
+        $this->addGroup($grp_footer);
 
         $field = DataInputFactory::Create(DataInputFactory::TEXT, "facebook_url", "Facebook URL", 0);
-        $this->addInput($field);
-
-        $field = DataInputFactory::Create(DataInputFactory::TEXT, "facebook_page_id", "Facebook Page ID", 0);
-        $this->addInput($field);
+        $this->addInput($field,$grp_footer);
 
         $field = DataInputFactory::Create(DataInputFactory::TEXT, "instagram_url", "Instagram URL", 0);
-        $this->addInput($field);
+        $this->addInput($field,$grp_footer);
 
         $field = DataInputFactory::Create(DataInputFactory::TEXT, "youtube_url", "Youtube URL", 0);
-        $this->addInput($field);
+        $this->addInput($field,$grp_footer);
 
-        $field = DataInputFactory::Create(DataInputFactory::TEXT, "maps_url", "Google Maps URL", 0);
-        $this->addInput($field);
+        $field = DataInputFactory::Create(DataInputFactory::TEXTAREA, "maps_url", "Google Maps URL", 0);
+        $this->addInput($field,$grp_footer);
+
+
+        $grp_pagefooter = new InputGroup("pageFooter", "Page Footer - Info Text");
+        $this->addGroup($grp_pagefooter);
 
         $field = DataInputFactory::Create(DataInputFactory::TEXTAREA, "phone_text", "Phone Text", 0);
-        $this->addInput($field);
+        $this->addInput($field, $grp_pagefooter);
 
         $field = DataInputFactory::Create(DataInputFactory::TEXTAREA, "email_text", "Email Text", 0);
-        $this->addInput($field);
+        $this->addInput($field, $grp_pagefooter);
 
         $field = DataInputFactory::Create(DataInputFactory::TEXTAREA, "address_text", "Address Text", 0);
-        $this->addInput($field);
+        $this->addInput($field, $grp_pagefooter);
 
         $field = DataInputFactory::Create(DataInputFactory::TEXTAREA, "working_hours_text", "Working hours Text", 0);
-        $this->addInput($field);
+        $this->addInput($field, $grp_pagefooter);
 
 
     }
