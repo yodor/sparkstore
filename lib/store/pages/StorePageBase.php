65a87c79705187f5401b8e8f054c0a1ca8178778
Todor Gyumyushev <yodor1@gmail.com>
2024-03-25 02:13:17 +0200

    updates: fix fb chat plugin delay loading

--- lib/store/pages/StorePageBase.php
+++ lib/store/pages/StorePageBase.php
@@ -610,31 +610,30 @@ class StorePageBase extends SparkPage
         <div id="fb-root"></div>
 
         <!-- Your Chat Plugin code -->
-        <div id="fb-customer-chat" class="fb-customerchat">
+        <div id="fb-customer-chat" class="fb-customerchat" page_id="<?php echo $page_id;?>" attribution="biz_inbox">
         </div>
-
-        <script>
-            var chatbox = document.getElementById('fb-customer-chat');
-            chatbox.setAttribute("page_id", "<?php echo $page_id;?>");
-            chatbox.setAttribute("attribution", "biz_inbox");
-        </script>
 
         <!-- Your SDK code -->
         <script>
-            window.fbAsyncInit = function() {
-                FB.init({
-                    xfbml            : true,
-                    version          : 'v15.0'
-                });
-            };
 
-            (function(d, s, id) {
+
+            var _x = function(d, s, id) {
+                window.fbAsyncInit = function() {
+                    FB.init({
+                        xfbml            : true,
+                        version          : 'v15.0'
+                    });
+                };
+
                 var js, fjs = d.getElementsByTagName(s)[0];
                 if (d.getElementById(id)) return;
                 js = d.createElement(s); js.id = id;
                 js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
                 fjs.parentNode.insertBefore(js, fjs);
-            }(document, 'script', 'facebook-jssdk'));
+            };
+
+            setTimeout(function() { _x(document, 'script', 'facebook-jssdk') }, 5000);
+
         </script>
         <?php
     }
