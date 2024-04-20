eba984a37275f0f5086f0830516ca8013fbafefb
Todor Gyumyushev <yodor1@gmail.com>
2024-04-20 21:59:44 +0300

    updates

--- lib/store/components/renderers/items/ProductDetailsItem.php
+++ lib/store/components/renderers/items/ProductDetailsItem.php
@@ -246,7 +246,7 @@ class ProductDetailsItem extends Component implements IHeadContents,  IPhotoRend
                 echo "<div class='item' itemClass='ProductPhotosBean' itemID='$ppID' pos='$pos' onClick='javascript:galleryItemClicked(this)' relation='ProductGallery' $active>";
                 if ($si instanceof StorageItem) {
                     $src = $si->hrefThumb(64);
-                    echo "<img src='$src' alt='$product_name' title='$product_name'>";
+                    echo "<img src='$src' alt='$product_name' loading='lazy' title='$product_name'>";
                 }
                 echo "</div>";
 
