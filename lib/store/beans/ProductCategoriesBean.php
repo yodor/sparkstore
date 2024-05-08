3b5733f7280866e20eb5f5dff34aa9ff6b7f57b5
Todor Gyumyushev <yodor1@gmail.com>
2024-05-08 19:47:14 +0300

    updates: seo description

--- lib/store/beans/ProductCategoriesBean.php
+++ lib/store/beans/ProductCategoriesBean.php
@@ -6,18 +6,21 @@ class ProductCategoriesBean extends NestedSetBean
 
     protected $createString = "CREATE TABLE `product_categories` (
   `catID` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `category_name` varchar(512) NOT NULL,
-  `category_keywords` text DEFAULT NULL,
+  `category_name` varchar(50) NOT NULL,
+  `href` text NOT NULL,
   `parentID` int(11) unsigned NOT NULL DEFAULT 0,
   `lft` int(11) unsigned NOT NULL,
   `rgt` int(11) unsigned NOT NULL,
-  `extpath` varchar(255) NOT NULL,
+  `category_keywords` text DEFAULT NULL,
+  `category_seotitle` text DEFAULT NULL,
+  `category_seodescription` varchar(512) DEFAULT NULL,
   PRIMARY KEY (`catID`),
   KEY `category_name` (`category_name`),
   KEY `parentID` (`parentID`),
   KEY `lft` (`lft`),
   KEY `rgt` (`rgt`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci";
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci
+";
 
     public function __construct()
     {
