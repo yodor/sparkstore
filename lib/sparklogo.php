ba45af6eef862b27b5a19a5f28eff0e7559137ac
Todor Gyumyushev <yodor1@gmail.com>
2024-09-07 21:39:12 +0300

    refactoring user uploaded logo

--- lib/sparklogo.php
+++ lib/sparklogo.php
@@ -1,25 +1,21 @@
 <?php
-define("SKIP_SESSION",1);
-define("SKIP_DB",1);
-define("SKIP_TRANSLATOR",1);
-
 include_once("utils/SparkFile.php");
 include_once("storage/SparkHTTPResponse.php");
 
-$user_logo = new SparkFile("logo");
+$user_logo = new SparkFile();
+$user_logo->setFilename("logo");
 $user_logo->setPath(CACHE_PATH);
 
-$origin_logo = new SparkFile(LOGO_NAME);
+$origin_logo = new SparkFile();
+$origin_logo->setFilename(LOGO_NAME);
 $origin_logo->setPath(LOGO_PATH);
 
 $response = new SparkHTTPResponse();
 if ($user_logo->exists()) {
     $response->sendFile($user_logo);
-
 }
 else {
     $response->sendFile($origin_logo);
 }
 exit;
-
 ?>
