c2d33305295a93e9c9b2c345a34cde379bf74a80
Todor Gyumyushev <yodor1@gmail.com>
2025-04-08 21:55:40 +0300

    refactoring

--- products/details.php
+++ products/details.php
@@ -1,36 +1,13 @@
 <?php
 include_once("session.php");
-include_once("store/components/renderers/items/ProductDetailsItem.php");
-
-include_once("store/pages/ProductDetailsPageBase.php");
+include_once("class/pages/ProductDetailsPage.php");
 
 $page = new ProductDetailsPageBase();
-
-$sellable = $page->getSellable();
-
-$cmp = new ProductDetailsItem($sellable);
-$cmp->setURL(URL::Current()->fullURL());
-$cmp->setCategories($page->getCategoryPath());
-$cmp->initialize();
+$page->initialize();
 
 $page->startRender();
 
-$page->renderCategoryPath();
-
-$cmp->render();
-
-
-echo "<div class='product_group same_category '>";
-$page->renderSameCategoryProducts();
-echo "</div>";
-
-//echo "<div class='product_group most_ordered'>";
-//$page->renderMostOrderedProducts();
-//echo "</div>";
-
-//echo "<div class='product_group most_ordered'>";
-//$page->renderOtherProducts();
-//echo "</div>";
+$page->renderContents();
 
 $page->finishRender();
 ?>
