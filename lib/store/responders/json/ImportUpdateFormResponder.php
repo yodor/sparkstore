555af211f5ebe43c3724954f45bcf96e697b671a
Todor Gyumyushev <yodor1@gmail.com>
2025-07-28 17:05:22 +0300

    fixes: skip UTF8 BOM

--- lib/store/responders/json/ImportUpdateFormResponder.php
+++ lib/store/responders/json/ImportUpdateFormResponder.php
@@ -26,61 +26,79 @@ class ImportUpdateFormResponder extends JSONFormResponder
             throw new Exception("Incorrect upload file");
         }
 
-
         $separator = ",";
         $enclosure = '"';
         $escape = "\\";
 
-        debug("Uploaded file: ".$file->data());
-
-        $stream = fopen('data://text/plain,' . $file->data(),'r');
+        $stream = fopen("data:text/plain," . $file->data(),"r");
         if (!$stream) throw new Exception("Unable to open uploaded file as stream");
-
-        $bean = new ProductsBean();
-
-        $db = DBConnections::Open();
 
         try {
 
-            $db->transaction();
-
-            $linePosition = 0;
-
-            while (($line = fgetcsv($stream, 0, $separator, $enclosure, $escape)) !== FALSE) {
-                $linePosition++;
-                if (count($line) != 3) throw new Exception("Incorrect number of columns. Expected 3 columns");
-
-                if ($linePosition == 1) {
-
-                    $key_prodID = preg_replace('/[[:^print:]]/', '', $line[0]);
-                    if (strcmp($key_prodID, "prodID") !== 0) {
-                        throw new Exception("Incorrect column keys name for prodID: ".$line[0]." size (".strlen($line[0]).")");
-                    }
-                    $key_productName = preg_replace('/[[:^print:]]/', '', $line[1]);
-                    if (strcmp($key_productName, "product_name") !== 0) {
-                        throw new Exception("Incorrect column keys name for product_name: ".$line[1]." size (".strlen($line[1]).")");
-                    }
-                    $key_productDescription = preg_replace('/[[:^print:]]/', '', $line[2]);
-                    if (strcmp($key_productDescription, "product_description") !== 0) {
-                        throw new Exception("Incorrect column keys name for product_description: ".$line[2]." size (".strlen($line[2]).")");
-                    }
-                    continue;
-                }
-
-                $prodID = (int)$line[0];
-                $productName = sanitizeInput(strip_tags($line[1]));
-                $productDescription = sanitizeInput(strip_tags($line[2]));
-                $updateData = array("product_name"=>$productName, "product_description"=>$productDescription);
-
-                $bean->update($prodID, $updateData, $db);
-
+            //skip BOM if any
+            if (fread($stream, 3) !== "\xEF\xBB\xBF") {
+                // if there is no BOM rewind
+                rewind($stream);
+            }
+            else {
+                debug("BOM UTF-8 skipped");
             }
 
-            $db->commit();
-            $resp->message = "Updated ".($linePosition-1)." products.";
+            //read key column names
+            $line = fgetcsv($stream, 0, $separator, $enclosure, $escape);
+
+            if (!is_array($line)) {
+                throw new Exception("Incorrect CSV format of file");
+            }
+
+            $keyNames = array(0 => "prodID", 1 => "product_name", 2 => "product_description");
+
+            if (count($line) != count($keyNames)) {
+                throw new Exception("Incorrect number of key columns");
+            }
+
+            for ($i = 0; $i < count($keyNames); $i++) {
+                if (strcmp($line[$i], $keyNames[$i]) !== 0) {
+                    throw new Exception("Incorrect column keys name: " . $line[$i]);
+                }
+            }
+
+
+            $bean = new ProductsBean();
+
+            $db = DBConnections::Open();
+
+            try {
+
+                $db->transaction();
+
+                $linePosition = 1;
+
+                while (($line = fgetcsv($stream, 0, $separator, $enclosure, $escape)) !== FALSE) {
+
+                    if (count($line) != 3) throw new Exception("Incorrect number of columns. Expected 3 columns");
+
+                    $prodID = (int)$line[0];
+                    $productName = sanitizeInput($line[1]);
+                    $productDescription = sanitizeInput($line[2]);
+                    $updateData = array("product_name" => $productName, "product_description" => $productDescription);
+
+                    $bean->update($prodID, $updateData, $db);
+
+                }
+
+                $db->commit();
+                $resp->message = "Updated " . $linePosition . " products.";
+
+                fclose($stream);
+
+            } catch (Exception $e) {
+                $db->rollback();
+                throw $e;
+            }
         }
         catch (Exception $e) {
-            $db->rollback();
+            fclose($stream);
             throw $e;
         }
 
