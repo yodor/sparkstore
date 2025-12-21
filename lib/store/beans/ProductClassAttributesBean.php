c9c8cbbb8d6a297be72d329c7781ce01c895f70c
Todor Gyumyushev <yodor1@gmail.com>
2025-12-21 22:59:59 +0200

    fixes

--- lib/store/beans/ProductClassAttributesBean.php
+++ lib/store/beans/ProductClassAttributesBean.php
@@ -5,15 +5,15 @@ class ProductClassAttributesBean extends DBTableBean
 {
 
     protected string $createString = "CREATE TABLE `product_class_attributes` (
-  `pcaID` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `pclsID` int(11) unsigned NOT NULL,
-  `attrID` int(11) unsigned NOT NULL,
-  PRIMARY KEY (`pcaID`),
-  UNIQUE KEY `class_attribute` (`pclsID`,`attrID`) USING BTREE,
-  KEY `pclsID` (`pclsID`),
-  KEY `attrID` (`attrID`),
-  CONSTRAINT `product_class_attributes_ibfk_1` FOREIGN KEY (`attrID`) REFERENCES `attributes` (`attrID`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `product_class_attributes_ibfk_2` FOREIGN KEY (`pclsID`) REFERENCES `product_classes` (`pclsID`) ON DELETE CASCADE ON UPDATE CASCADE
+`pcaID` int(11) unsigned NOT NULL AUTO_INCREMENT,
+`pclsID` int(11) unsigned NOT NULL,
+`attrID` int(11) unsigned NOT NULL,
+PRIMARY KEY (`pcaID`),
+UNIQUE KEY `class_attribute` (`attrID`,`pclsID`),
+KEY `pclsID` (`pclsID`),
+KEY `attrID` (`pcaID`),
+CONSTRAINT `product_class_attributes_ibfk_10` FOREIGN KEY (`attrID`) REFERENCES `attributes` (`attrID`) ON UPDATE CASCADE,
+CONSTRAINT `product_class_attributes_ibfk_9` FOREIGN KEY (`pclsID`) REFERENCES `product_classes` (`pclsID`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci";
 
     public function __construct()
