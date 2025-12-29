15bc5f2485e1aa02f93f94c7c61a8e870449be36
Todor Gyumyushev <yodor1@gmail.com>
2025-12-29 22:18:20 +0200

    updates

--- lib/store/beans/ProductViewLogBean.php
+++ lib/store/beans/ProductViewLogBean.php
@@ -9,6 +9,10 @@ class ProductViewLogBean extends DBTableBean
   `order_counter` int(11) unsigned NOT NULL,
   `update_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   UNIQUE KEY `prodID` (`prodID`),
+  KEY `update_date` (`update_date`),
+  KEY `view_counter` (`view_counter`),
+  KEY `prodID_2` (`prodID`,`view_counter`),
+  KEY `prodID_3` (`prodID`,`order_counter`),
   CONSTRAINT `product_view_log_ibfk_1` FOREIGN KEY (`prodID`) REFERENCES `products` (`prodID`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci
 
