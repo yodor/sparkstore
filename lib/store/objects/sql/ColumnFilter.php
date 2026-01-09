13a53e59ec237bdea86ab50daf6efd0e99498c00
Todor Gyumyushev <yodor1@gmail.com>
2026-01-09 12:22:46 +0200

    fixes

--- lib/store/objects/sql/ColumnFilter.php
+++ lib/store/objects/sql/ColumnFilter.php
@@ -6,7 +6,7 @@ class ColumnFilter extends ClosureFilter
     public function __construct(string $title, int $matchMode = ClosureFilter::MATCH_LIKE)
     {
         $closure = function(SQLSelect $select, DataInput $input){
-            $select->where()->add($input->getName(), $this->getMatchValue($input), $this->getMatchOperator($input));
+            $select->where()->add($input->getName(), "'{$this->getMatchValue($input)}'", $this->getMatchOperator($input));
         };
         parent::__construct($title, $closure);
         $this->matchMode = $matchMode;
