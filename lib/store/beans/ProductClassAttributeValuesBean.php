01daf1c047746f41464e8463b6f1cbf95631946e
Todor Gyumyushev <yodor1@gmail.com>
2026-01-13 13:17:43 +0200

    adjust indexes on product attributes

--- lib/store/beans/ProductClassAttributeValuesBean.php
+++ lib/store/beans/ProductClassAttributeValuesBean.php
@@ -7,14 +7,17 @@ class ProductClassAttributeValuesBean extends DBTableBean
   `pcavID` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `prodID` int(11) unsigned NOT NULL,
   `pcaID` int(11) unsigned NOT NULL,
-  `value` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`pcavID`),
-  UNIQUE KEY `product_value` (`prodID`,`pcaID`),
-  KEY `prodID` (`prodID`),
+  `value` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`pcavID`) USING BTREE,
+  UNIQUE KEY `product_attributes` (`prodID`,`pcaID`),
   KEY `pcaID` (`pcaID`),
-  CONSTRAINT `product_class_attribute_values_ibfk_1` FOREIGN KEY (`pcaID`) REFERENCES `product_class_attributes` (`pcaID`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `product_class_attribute_values_ibfk_2` FOREIGN KEY (`prodID`) REFERENCES `products` (`prodID`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci";
+  KEY `prodID` (`prodID`),
+  KEY `pcaID_2` (`pcaID`,`value`),
+  KEY `prodID_2` (`prodID`,`pcaID`),
+  FULLTEXT KEY `value` (`value`),
+  CONSTRAINT `product_class_attribute_values_ibfk_1` FOREIGN KEY (`prodID`) REFERENCES `products` (`prodID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `product_class_attribute_values_ibfk_2` FOREIGN KEY (`pcaID`) REFERENCES `product_class_attributes` (`pcaID`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci";
 
     public function __construct()
     {
--- lib/store/beans/ProductClassAttributesBean.php
+++ lib/store/beans/ProductClassAttributesBean.php
@@ -5,16 +5,17 @@ class ProductClassAttributesBean extends DBTableBean
 {
 
     protected string $createString = "CREATE TABLE `product_class_attributes` (
-`pcaID` int(11) unsigned NOT NULL AUTO_INCREMENT,
-`pclsID` int(11) unsigned NOT NULL,
-`attrID` int(11) unsigned NOT NULL,
-PRIMARY KEY (`pcaID`),
-UNIQUE KEY `class_attribute` (`attrID`,`pclsID`),
-KEY `pclsID` (`pclsID`),
-KEY `attrID` (`pcaID`),
-CONSTRAINT `product_class_attributes_ibfk_10` FOREIGN KEY (`attrID`) REFERENCES `attributes` (`attrID`) ON UPDATE CASCADE,
-CONSTRAINT `product_class_attributes_ibfk_9` FOREIGN KEY (`pclsID`) REFERENCES `product_classes` (`pclsID`) ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci";
+  `pcaID` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `pclsID` int(11) unsigned NOT NULL,
+  `attrID` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`pcaID`),
+  UNIQUE KEY `attrID_2` (`attrID`,`pclsID`),
+  KEY `pclsID` (`pclsID`),
+  KEY `attrID` (`pcaID`),
+  KEY `pcaID` (`pcaID`,`attrID`),
+  CONSTRAINT `product_class_attributes_ibfk_10` FOREIGN KEY (`attrID`) REFERENCES `attributes` (`attrID`) ON UPDATE CASCADE,
+  CONSTRAINT `product_class_attributes_ibfk_9` FOREIGN KEY (`pclsID`) REFERENCES `product_classes` (`pclsID`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci";
 
     public function __construct()
     {
