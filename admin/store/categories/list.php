a7f6dc130f14876fa1ff237d98b40801e8ce9e5a
Todor Gyumyushev <yodor1@gmail.com>
2026-02-10 15:08:32 +0200

    fixes

--- admin/store/categories/list.php
+++ admin/store/categories/list.php
@@ -16,14 +16,16 @@ $iterator = $view->getIterator();
 if ($iterator instanceof SQLQuery) {
     $iterator->select->fields()->setExpression("(SELECT pcp.pcpID FROM product_category_photos pcp WHERE pcp.catID = node.catID ORDER BY pcp.position ASC LIMIT 1)", " pcpID ");
 }
+//echo $iterator->select->getSQL();
 $item = $view->getItemRenderer();
 if ($item instanceof TextTreeItem) {
+
     $si = new StorageItem();
     $si->className = "ProductCategoryPhotosBean";
     $si->setName("pcpID");
-
+//
     $item->icon()->setStorageItem($si);
-    $item->icon()->setPhotoSize(-1, 32);
+    $item->icon()->setPhotoSize(0, 32);
 
     //banners for each category
     $item->getActions()->append(new Action("Banners Gallery", "banners/list.php", array(new DataParameter("catID", $cmp->getBean()->key()))));
