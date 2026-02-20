257a4ccbd5edfd57f7da5f6e1c4aaf83653e014e
Todor Gyumyushev <yodor1@gmail.com>
2026-02-20 17:07:09 +0200

    refactoring

--- lib/store/pages/StorePageBase.php
+++ lib/store/pages/StorePageBase.php
@@ -103,7 +103,7 @@ class StorePageBase extends SparkPage
 
         $googleID_analytics = $config->get("googleID_analytics");
         if ($googleID_analytics) {
-            $gtag = new GTAG($googleID_analytics);
+            $gtag = new GTAG();
             $this->head()->addScript($gtag);
 
             $cmd = new GTMCommand();
@@ -114,7 +114,7 @@ class StorePageBase extends SparkPage
 
         $googleID_ads = $config->get("googleID_ads");
         if ($googleID_ads) {
-            $gtag = new GTAG($googleID_ads);
+            $gtag = new GTAG();
             $this->head()->addScript($gtag);
 
             $cmd = new GTMCommand();
