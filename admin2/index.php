268722031500bf7263ef2e4d73a4760c85faca3b
Todor Gyumyushev <yodor1@gmail.com>
2026-03-05 22:22:50 +0200

    refactoring admin v2

--- admin2/index.php
+++ admin2/index.php
@@ -2,13 +2,4 @@
 include_once("templates/Template.php");
 
 Template::ModuleInit(Spark::GetObject(Config::MODULE_ADMIN));
-
 Template::ModuleResponse();
-
-//include_once("store/pages/AdminTemplatePageBase.php");
-//
-//$page = new AdminTemplatePageBase();
-//
-//$page->initialize();
-//
-//$page->render();
